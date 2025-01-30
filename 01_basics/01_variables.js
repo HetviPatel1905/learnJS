c115be6c8d97e1cb997b1c1bb5fe2138811f7a14
Hetvip <pathe1905@gmail.com>
2025-01-30 15:26:53 -0600

    data types, variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountName = 144553;
+let accountEmail = "hetvi@gmail.com";
+
+var accountPassword = "12345"; // not recommended
+accountyCity = "Jaiput"; // not recommended
+let accounState; // undefined
+
+// accountName = 2; // const can't be changed
+accountEmail = "hc@hc.com";
+
+accountPassword = "223344";
+accountyCity = "Gujarat";
+
+console.table([
+  accountName,
+  accountEmail,
+  accountPassword,
+  accountyCity,
+  accounState,
+]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,6 @@
+let name = "Hetvi"; // string
+let age = 22; // number
+let isLoggin = false; // boolean
+// null = standalone value, type of object
+// undefined = not defined value
+// symbol =
